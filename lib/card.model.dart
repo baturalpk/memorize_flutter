43a01ead749257f247d2fbcc19e5e3c0bc03a45d
Baturalp KIZILTAN <baturalp.kiziltan@outlook.com>
2021-07-09 21:22:08 +0300

    Describe the model Card

---
+++ lib/card.model.dart
@@ -0,0 +1,23 @@
+class EmojiCard {
+  late final int _id;
+  late final String _content;
+  late bool _isFaceUp;
+  late bool _isMatched;
+
+  EmojiCard({required int? id, required String? content}) {
+    this._id = id!;
+    this._content = content!;
+    this._isFaceUp = false;
+    this._isMatched = false;
+  }
+
+  // Getters
+  int get id => this._id;
+  String get content => this._content;
+  bool get isFaceUp => this._isFaceUp ;
+  bool get isMatched => this._isMatched;
+
+  // Setters
+  set setFaceUp(bool predicate) => this._isFaceUp = predicate;
+  set setMatched(bool predicate) => this._isMatched = predicate;
+}
