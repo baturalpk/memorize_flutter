1a2a2f9f8ae5a70d193090207b0b62766f02b68b
Baturalp KIZILTAN <baturalp.kiziltan@outlook.com>
2021-07-09 21:18:37 +0300

    Implement entry-point of the app

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:memorize_flutter/cards.notifier.dart';
+import 'package:memorize_flutter/gameGrid.view.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => MyCards(),
+      child: MaterialApp(
+        title: 'Memorize',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: GameGrid(),
+      ),
+    );
+  }
+}
