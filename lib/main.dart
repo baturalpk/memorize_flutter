472053523238750aa74ee276c68971090d0f2d34
baturalp-kiziltan <baturalp.kiziltan@outlook.com>
2021-07-13 18:05:09 +0300

    Set visibility of debug-banner false

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.red,
         ),
         home: GameGrid(),
+        debugShowCheckedModeBanner: false,
       ),
     );
   }
